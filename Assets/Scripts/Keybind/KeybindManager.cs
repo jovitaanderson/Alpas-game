0dbdb7dc0b459b155767885fa2397ce0097ad01c
Jovita Anderson <jovita@live.com.sg>
2022-06-30 17:04:46 +0800

    Avoid duplicate keybinds

--- Assets/Scripts/Keybind/KeybindManager.cs
+++ Assets/Scripts/Keybind/KeybindManager.cs
@@ -1,53 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class KeybindManager : MonoBehaviour
 {
+    [SerializeField] Text errorMessage;
     private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
 
-    public Text up, left, down, right, openMenu, confirm, goBack;
-
     private GameObject currentKey;
+    private GameObject[] keybindButtons;
 
-    private Color32 normal = new Color32(39, 171, 249, 255);
-    private Color32 selected = new Color32(239, 116, 36, 255);
+    private Color32 normal = new Color32(255, 255, 255, 255);
+    //private Color32 selected = new Color32(239, 116, 36, 255);
 
     // Start is called before the first frame update
     void Start()
     {
-        keys.Add("Up", KeyCode.W);
-        keys.Add("Left", KeyCode.A);
-        keys.Add("Down", KeyCode.S);
-        keys.Add("Right", KeyCode.D);
+        keybindButtons = GameObject.FindGameObjectsWithTag("Keybind");
 
-        up.text = keys["Up"].ToString();
-        down.text = keys["Down"].ToString();
-        left.text = keys["Left"].ToString();
-        right.text = keys["Right"].ToString();
+        keys.Add("UP", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("UP", "W")));
+        keys.Add("LEFT", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("LEFT", "A")));
+        keys.Add("DOWN", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("DOWN", "S")));
+        keys.Add("RIGHT", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("RIGHT", "D")));
+        keys.Add("SPRINT", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("SPRINT", "RightShift")));
 
-    }
+        keys.Add("MENU", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("MENU", "M")));
+        keys.Add("CONFIRM", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("CONFIRM", "Return")));
+        keys.Add("BACK", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("BACK", "Escape")));
+        errorMessage.text = "";
+        updateAllKeyText();
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(keys["Up"]))
-        {
-            Debug.Log("Up");
-        }
-        if (Input.GetKeyDown(keys["Down"]))
-        {
-            Debug.Log("Down");
-        }
-        if (Input.GetKeyDown(keys["Left"]))
-        {
-            Debug.Log("Left");
-        }
-        if (Input.GetKeyDown(keys["Right"]))
-        {
-            Debug.Log("Right");
-        }
     }
 
     private void OnGUI()
@@ -57,12 +42,32 @@ public class KeybindManager : MonoBehaviour
             Event e = Event.current;
             if (e.isKey)
             {
+                if (keys.ContainsValue(e.keyCode))
+                {
+                    string myKey = keys.FirstOrDefault(x => x.Value == e.keyCode).Key;
+                    keys[myKey] = KeyCode.None;
+                    updateKeyText(myKey, keys[myKey]);
+
+                }
+
                 keys[currentKey.name] = e.keyCode;
-                currentKey.transform.GetChild(0).GetComponent<Text>().text = e.keyCode.ToString();
+                updateKeyText(currentKey.name, keys[currentKey.name]);
                 currentKey.GetComponent<Image>().color = normal;
                 currentKey = null;
             }
         }
+    }
+
+    public void updateAllKeyText()
+    {
+        foreach (var keyButton in keybindButtons)
+            keyButton.GetComponentInChildren<Text>().text = keys[keyButton.name].ToString();
+    }
+
+    public void updateKeyText(string key, KeyCode code)
+    {
+        Text temp = Array.Find(keybindButtons, x => x.name == key).GetComponentInChildren<Text>();
+        temp.text = code.ToString();
     }
 
     public void ChangeKey(GameObject clicked)
@@ -72,6 +77,24 @@ public class KeybindManager : MonoBehaviour
             currentKey.GetComponent<Image>().color = normal;
         }
         currentKey = clicked;
-        currentKey.GetComponent<Image>().color = selected;
+        currentKey.GetComponent<Image>().color = GlobalSettings.i.HighlightedColor;
+    }
+
+    public void SaveKeys()
+    {
+        if (keys.ContainsValue(KeyCode.None))
+        {
+            errorMessage.text = "cannot save, all keys must be binded";
+            Debug.Log("cannot save, all keys must be binded");
+        }
+        else
+        {
+            errorMessage.text = "";
+            foreach (var key in keys)
+            {
+                PlayerPrefs.SetString(key.Key, key.Value.ToString());
+            }
+            PlayerPrefs.Save();
+        }
     }
 }
