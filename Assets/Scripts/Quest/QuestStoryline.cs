98dd60bdd6d6a091100e6d04c982c7ebfa3f45d9
Jovita Anderson <jovita@live.com.sg>
2022-07-19 15:12:02 +0800

    Updated mainmenu logo

--- Assets/Scripts/Quest/QuestStoryline.cs
+++ Assets/Scripts/Quest/QuestStoryline.cs
@@ -8,32 +8,19 @@ public class QuestStoryline : MonoBehaviour
 {
     [SerializeField] QuestBase[] storyline;
     [SerializeField] Text questDescriptionText;
-    int time = 0;
-    int currentQuestNo = 0;
-    //public event Action OnQuestCompleted;
-    List<QuestBase> questBase = new List<QuestBase>();
-    List<Quest> quest = new List<Quest>();
 
-    // Start is called before the first frame update
     QuestList questList;
+    List<Quest> quest = new List<Quest>();
+    List<QuestBase> questBase = new List<QuestBase>();
+
+    int currentQuestNo = 0;
+
     void Start()
     {
         questList = QuestList.GetQuestList();
         questList.OnUpdated += UpdateQuestDescription;
 
-        quest = questList.Quests;
-        QuestListToBaseQuestArray(questList);
         SetFirstQuestDescription();
-    }
-
-    private void Update()
-    {
-        time++;
-        if (time > 50)
-        {
-            Debug.Log($"IN UPDATE: Quest count: {quest.Count}, QuestBase count:{questBase.Count}");
-            time = 0;
-        }
     }
 
     private void RefreshList()
@@ -58,31 +45,23 @@ public class QuestStoryline : MonoBehaviour
         quest = questList.Quests;
         if (questBase.Contains(quests))
         {
-
             int idx = questBase.IndexOf(quests);
             if (idx >= 0)
             {
                 if (quest[idx].Status == QuestStatus.Started)
                     return true;
             }
-            return false;
         }
-        else
-            return false;
+        return false;
     }
 
     public void SetFirstQuestDescription()
     {
         RefreshList();
-        Debug.Log($"Quest count: {quest.Count}, QuestBase count:{questBase.Count}");
         for (int i = currentQuestNo; i < storyline.Length; i++)
         {
             if (!questBase.Contains(storyline[i]) || isQuestIncomplete(storyline[i]))
             {
-                if (!questBase.Contains(storyline[i]))
-                    Debug.Log($"questBase dont have {storyline[i].Name}");
-                if (isQuestIncomplete(storyline[i]))
-                    Debug.Log($"{storyline[i].Name} is incomplete");
                 questDescriptionText.text = storyline[i].Description;
                 break;
             }
